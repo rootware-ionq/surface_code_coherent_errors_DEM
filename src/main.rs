fee96f7b93ef76c409e879e9f0f8daae48e14ccc
Shah Saad Alam <shahsaad.alam@ionq.co>
2026-02-22 02:26:46 -0700

    clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,5 @@ fn main() {
     let theta = 0.1 * std::f64::consts::PI;
     let coherent_error_state = apply_coherent_error(logical_plus_state, theta);
 
-    println!("coherent_error_state (index [binary]\tnorm^2):");
-
-    let mut sum = 0.0;
-    for i in 0..coherent_error_state.nrows() {
-        let v = coherent_error_state.as_ref().get(i, 0);
-        let norm_sq = v.re * v.re + v.im * v.im;
-
-        if norm_sq > 1e-10 {
-            println!("{i:09b}\t{norm_sq:.10}");
-        }
-        sum += norm_sq;
-    }
-    println!("1.0 - sum of coeff:\t{:e}", 1.0 - sum);
-
     perform_x_ancilla_checks(&coherent_error_state);
 }
